10dda8df2a3df8ee602297bedb5bad57de6b6f09
Łukasz Arczyński <lukaszarczynski@gmail.com>
2017-09-21 05:49:12 +0200

    Meaning is now exactly one part of speech

--- morphosyntactic.py
+++ morphosyntactic.py
@@ -1,6 +1,10 @@
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 import sys
-from typing import List
+from typing import (
+    List,
+    Set,
+    Tuple
+)
 
 CONSOLE_WIDTH = 80
 DICT_LEN = 4811854
@@ -26,14 +30,14 @@ def progress_bar(console_width=CONSOLE_WIDTH):
 
 class Meaning():
     """Represents one of possible meanings of given word,
-    where meaning is map from given word to base word"""
+    where meaning is map from given word to base word,
+    and there is exactly one part of speech associated with this meaning"""
 
-    # TODO: which is single part of speech
     def __init__(self, word: str, base_word: str, tags: List[str]):
         self.word = word
         self.base_word = base_word
         self.unfiltered_tags = Meaning.fix_tags(tags)
-        self.part_of_speech = ...
+        self.part_of_speech = self.unfiltered_tags[0][0]
 
     def __str__(self):
         return "(meaning: {0}, base_word: {1}, unfiltered_tags: {2})".format(self.word, self.base_word, self.unfiltered_tags)
@@ -57,13 +61,17 @@ class Noun(Meaning):
     ...
 
 
-class AmbiguousWord():
+class AmbiguousWord:
     """Stores multiple possible meanings of given word"""
-    def __init__(self, word: str, meanings: List):
-        self.word = word
+    def __init__(self, original_word: str, meanings: List):
+        self.word = original_word
         self.meanings = []  # type: List[Meaning]
-        for word, base_word, tags in meanings:
-            meaning = Meaning(word, base_word, tags)
+        fixed_meanings = []
+        for raw_meaning in meanings:
+            fixed_meanings += AmbiguousWord.split_by_parts_of_speech(raw_meaning)
+
+        for raw_meaning in fixed_meanings:
+            meaning = Meaning(*raw_meaning)
             self.meanings.append(meaning)
 
     def __str__(self):
@@ -81,11 +89,42 @@ class AmbiguousWord():
             tags += meaning.unfiltered_tags
         return tags
 
-    def parts_of_speech(self) -> List[str]:
-        ...
+    @staticmethod
+    def get_parts_of_speech_from_meaning(meaning: Tuple[str, str, Tuple]) -> Set[str]:
+        """Returns parts of speect occuring in raw list of meanings"""
+        tags = meaning[2]
+        parts_of_speech = {tag.split(":")[0] for tag in tags}
+        return parts_of_speech
+
+    @staticmethod
+    def split_by_parts_of_speech(meaning: Tuple[str, str, Tuple]) -> List[Tuple[str, str, Tuple]]:
+        """Splits raw meaning into list of raw meanings,
+        where each tag of each meaning has exactly one part of speech"""
+
+        def get_tags_with_given_part_of_speech(pos) -> List[str]:
+            tags = []
+            for tag in meaning[2]:
+                if tag.split(":")[0] == pos:
+                    tags.append(tag)
+            return tags
+
+        parts_of_speech = AmbiguousWord.get_parts_of_speech_from_meaning(meaning)
+        if len(parts_of_speech) == 1:
+            return [meaning]
+
+        word, base_word, _ = meaning
+        splitted_meanings = []
+        for part_of_speech in parts_of_speech:
+            classified_tags = get_tags_with_given_part_of_speech(part_of_speech)
+            splitted_meanings.append((word, base_word, tuple(classified_tags)))
+        return splitted_meanings
+
+    def parts_of_speech(self) -> Set[str]:
+        """Returns part of speech of every stored meaning of ambiguous word"""
+        return {meaning.part_of_speech for meaning in self.meanings}
 
 
-class Morphosyntactic():
+class Morphosyntactic:
     def __init__(self, dictionary_file_path):
         self.morphosyntactic_dictionary = defaultdict(lambda: [])
         self.dictionary_file_path = dictionary_file_path
@@ -128,5 +167,28 @@ if __name__ == "__main__":
     assert ['subst', 'pl', 'gen', 'n2'] in sample_meaning.unfiltered_tags
     sample_word = AmbiguousWord("picie", d["picie"])
     print(sample_word)
-    assert Meaning("picie", "pita", [":".join(['subst', 'sg', 'dat', 'f']),
-                                     ":".join(['subst', 'sg', 'loc', 'f'])]) in sample_word.meanings
+    assert "pita" in [meaning.base_word for meaning in sample_word.meanings]
+    assert [
+               ['subst', 'sg', 'dat', 'f'],
+               ['subst', 'sg', 'loc', 'f']
+           ] in [meaning.unfiltered_tags for meaning in sample_word.meanings]
+    sample_raw_meaning = ('pić', 'picie', ('subst:pl:gen:n2',))
+    sample_raw_mixed_meaning = (
+        'czerwony', 'czerwony',
+        ('adj:sg:acc:m3:pos', 'adj:sg:nom.voc:m1.m2.m3:pos', 'subst:sg:nom:m1', 'subst:sg:voc:m1'))
+    print(AmbiguousWord.get_parts_of_speech_from_meaning(sample_raw_mixed_meaning))
+    assert AmbiguousWord.get_parts_of_speech_from_meaning(sample_raw_mixed_meaning) == {"adj", "subst"}
+    assert AmbiguousWord.split_by_parts_of_speech(sample_raw_meaning) == [sample_raw_meaning]
+    print(AmbiguousWord.split_by_parts_of_speech(sample_raw_mixed_meaning))
+    print(AmbiguousWord.split_by_parts_of_speech(sample_raw_mixed_meaning)[0][-1])
+    assert set(AmbiguousWord.split_by_parts_of_speech(sample_raw_mixed_meaning)) == {
+        ('czerwony', 'czerwony', ('adj:sg:acc:m3:pos', 'adj:sg:nom.voc:m1.m2.m3:pos')),
+        ('czerwony', 'czerwony', ('subst:sg:nom:m1', 'subst:sg:voc:m1'))
+    }
+    print(Meaning(*sample_raw_meaning).part_of_speech)
+    assert Meaning(*sample_raw_meaning).part_of_speech == "subst"
+
+    tricky_word = AmbiguousWord("czerwony", [sample_raw_mixed_meaning])
+    print(tricky_word)
+    assert tricky_word.parts_of_speech() == {"adj", "subst"}
+    assert {meaning.base_word for meaning in tricky_word.meanings} == {"czerwony"}

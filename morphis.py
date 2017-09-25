e06944a0c6bc398876bb2eedff6ec695ff4ec440
Łukasz Arczyński <lukaszarczynski@gmail.com>
2017-09-25 20:02:31 +0200

    Get grammar category of replacement word, add debug mode

--- morphis.py
+++ morphis.py
@@ -2,10 +2,14 @@ import math
 from os.path import isfile
 from decimal import Decimal
 import decimal
-from typing import List
+from typing import List, Dict, Tuple
 
 import tokenization
 import morphosyntactic
+import grammar_category
+import replacing
+
+DEBUG = False
 
 
 def are_you_human():
@@ -20,8 +24,11 @@ def are_you_human():
         print("Dobrze, jesteś człowiekiem")
     elif math.isclose(solution, 0.3, abs_tol=1e-15):
         print("WITAMY W TAJNYM INTERNECIE ROBOTÓW")
+        global DEBUG
+        DEBUG = True
     else:
         print("Nawet się nie starasz...")
+
 
 def read_copypasta() -> List[str]:
     """Reads copypasta from input or from file, returns arrays of tokens and non-alphanumeric strings"""
@@ -39,12 +46,47 @@ def read_copypasta() -> List[str]:
 
     if len(lines) == 1 and isfile(lines[0].rstrip("\n").strip('"')):
         with open(lines[0].rstrip("\n").strip('"')) as file:
-            pasta = "".join(file.readlines())
+            copypasta = "".join(file.readlines())
     else:
-        pasta = "\n".join(lines)
+        copypasta = "\n".join(lines)
 
-    pasta = tokenization.tokenize(pasta)
-    return pasta
+    copypasta = tokenization.tokenize(copypasta)
+    return copypasta
+
+
+def choose_words() -> List[Tuple[Dict, grammar_category.Gender, float]]:
+    word = {
+        grammar_category.Number.SINGULAR: {},
+        grammar_category.Number.PLURAL: {}
+    }
+    gender = None
+    while gender is None:
+        print("Jakiego rodzaju rzeczowniki chcesz zamienić?",
+              "m1 -- męski, osobowy",
+              "m2 -- męski, ożywiony",
+              "m3 -- męski, nieożywiony",
+              "f -- żeński",
+              "n -- nijaki", sep="\n")
+        gender = grammar_category.gender_shortcuts.get(input(">"))
+    print("Podaj rzeczownik tego rodzaju")
+    word[grammar_category.Number.SINGULAR][grammar_category.Case.NOMINATIVE] = input()
+    print("Liczba pojedyncza")
+    get_word_cases(word, number=grammar_category.Number.SINGULAR)
+    print("Liczba mnoga")
+    get_word_cases(word, number=grammar_category.Number.PLURAL)
+    probability = -1.
+    while probability < 0 or probability > 1:
+        probability = float(input("Z jakim prawdopodobieństwem zamienić słowo? [0-1] "))
+    return [(word, gender, probability)]
+
+
+def get_word_cases(word, *, number):
+    for case_shortcut in grammar_category.cases_order:
+        case = grammar_category.case_shortcuts[case_shortcut]
+        if case in word[number]:
+            print(grammar_category.case_to_questions[case_shortcut], ": ", word[number][case])
+        else:
+            word[number][case] = input(grammar_category.case_to_questions[case_shortcut] + ": ")
 
 
 def find_morphosyntactic() -> str:
@@ -81,8 +123,18 @@ if __name__ == "__main__":
     morph = morphosyntactic.Morphosyntactic(morph_path)
     morph.create_morphosyntactic_dictionary()
 
-    pasta = read_copypasta()
-    print(pasta)
-    for token in pasta:
-        if token.isalnum():
-            print(token, "=>", morph.morphosyntactic_dictionary[token.lower()])
+    should_continue = True
+    while should_continue:
+        pasta = read_copypasta()
+        if "".join(pasta).strip() == "":
+            should_continue = False
+            break
+        if DEBUG:
+            print(pasta)
+            for token in pasta:
+                if token.isalnum():
+                    print(token, "=>", len(morph.morphosyntactic_dictionary.get(token.lower(), [])))
+
+        chosen_words = choose_words()
+        if DEBUG:
+            print(chosen_words)
